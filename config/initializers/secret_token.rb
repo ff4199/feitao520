aea01c3f5f87a9b67c64d2c75cd3db10147bd2ae
ff4199 <yangfeiyuok@gmail.com>
2013-06-10 18:50:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feitao520::Application.config.secret_token = 'e199c07a1640f905f32ba70b186c9f63e0318e1a941d91f3b95ea0296b4b1628b28a266217dda83b10970194048f633bc913d9ae76d303cb765f7817c8f4deb1'
