24c53afe10a041991194a1439636d0024cb0cda0
ff4199 <yangfeiyuok@gmail.com>
2013-06-23 10:29:08 +0800

    beat1.1

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
   attr_accessible :introduce, :name, :url
   has_many :attachments, :as => :attachmentable
+  default_scope order: 'pictures.created_at DESC'
 end
